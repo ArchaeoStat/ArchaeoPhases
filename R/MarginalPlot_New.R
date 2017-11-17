10b913627f1c25824d2d2a5916c3c40ec2aca499
vibet <vibet@MacBook-Air-de-vibet.local>
2017-11-17 18:03:07 +0100

    Vignettes and Update of MarginalPlot

---
+++ R/MarginalPlot_New.R
@@ -0,0 +1,85 @@
+#####################################################
+#          Marginal posterior Density               #
+#       NEW version in ArchaeoPhases 1.4            #
+#####################################################
+#' Marginal posterior Density
+#'
+#' Plots the density of a_chain + statistics (mean, CI, HPDR)
+#'
+#' @param data dataframe containing the output of the MCMC algorithm
+#' @param position numeric vector containing the position of the column corresponding to the MCMC chains of interest
+#' @param level probability corresponding to the level of confidence
+#' @param GridLength length of the grid used to estimate the density
+#' @param title title of the graph
+#' @param subtitle subtitle of the graph
+#' @param caption caption of the graph
+#' @param labelXaxis label of the x-axis
+#' @param labelYaxis label of the y-axis
+#' @param width width size in units
+#' @param height height size in units
+#' @param units a string recognized by the ggsave function, one of "in", "cm", "mm"
+#' @param x.min minimum x axis value
+#' @param x.max maximum x axis value
+#' @param x.scale one of "calendar" for calendar years, "BP" for years before present, or "elapsed" for time elapsed from a specified origin
+#' @param elapsed.origin.position position of the column to use as the origin for elapsed time calculations
+#' @param y.grid switch for horizontal grid lines
+#' @param file the name of the graph (+ extension) that will be saved if chosen. Null by default.
+#' @param newWindow whether the plot is drawn within a new window or not
+#' @return a plot
+#' @export
+#' 
+MarginalPlot <- function(a_chain, level=0.95, title="Characteristics of a date", subtitle = NULL,
+                          GridLength=1024,
+                          caption = "ArchaeoPhases",
+                          x.label = "Calendar year",y.label = NULL,
+                          color ="black",
+                          x.scale = "calendar", elapsed.origin.position = NULL,
+                          x.min = NULL, x.max = NULL, 
+                          y.grid = FALSE,
+                          height = 7, width = 7, units = "in",
+                          file = NULL, newWindow=TRUE){
+  
+  # x.scale can either be "calendar", "BP" or "elapsed" if any other origin that 0 and 1950
+  if (x.scale == "BP") {
+    a_chain <- 1950-a_chain
+  }
+  if (x.scale == "elapsed") {
+    a_chain <- elapsed.origin.position-a_chain
+  }
+  # credible interval
+  CR <- CredibleInterval(a_chain, level)
+  CR <- c(CR,"y"=0,"yend"=0)
+  # Mean 
+  Mean = mean(a_chain)
+  # new dataframe
+  dataCR = data.frame("Inf" = CR[2], "Sup" = CR[3], "y"=0,"yend"=0, "Mean"=Mean)
+  
+  h <- ggplot2::ggplot(data = data, ggplot2::aes(x=a_chain))
+  h <- h + ggplot2::geom_density(n = GridLength)
+  h <- h + ggplot2::geom_segment(data = dataCR, aes(x=dataCR[1,1], xend=dataCR[1,2], y = dataCR[1,3], yend = dataCR[1,4], colour = '#E7B8OO'), size=3.95, show.legend=F) 
+  h <- h + ggplot2::geom_point(data = dataCR,aes(x=Mean, y = dataCR[1,3]), size = 2)
+  h <- h + scale_color_manual(values =c('#56B4E9', '#FC4EO7'))
+  h <- h + ggplot2::labs(x = x.label,y = y.label,title = title,subtitle = subtitle, caption = caption)
+  
+  if (!y.grid) {
+    h <- h + ggplot2::theme(panel.grid.major.y=ggplot2::element_blank())
+  }
+  # x abscisses
+  if (is.null(x.min) ) {
+    x.min <- min(density(a_chain, n=GridLength)$x)
+  }
+  if (is.null(x.max)) {
+    x.max <- max(density(a_chain, n=GridLength)$x)
+  }
+  h <- h + ggplot2::xlim(x.min, x.max)
+  
+  # export file
+  if (!is.null(file)) {
+    ggplot2::ggsave(filename = file, plot = h, height = height,width = width, units = units)
+  }
+  if(newWindow == TRUE) {
+    dev.new(height = height, width = width)
+  }
+  print(h)
+  
+}
