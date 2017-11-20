cad477800b7d1a9a95293e811ccff5b7cdd062bb
vibet <vibet@MacBook-Air-de-vibet.local>
2017-11-20 09:44:56 +0100

    last updates

--- R/MarginalPlot.R
+++ R/MarginalPlot.R
@@ -1,89 +1,84 @@
 #####################################################
 #          Marginal posterior Density               #
+#       NEW version in ArchaeoPhases 1.4            #
 #####################################################
 #' Marginal posterior Density
 #'
 #' Plots the density of a_chain + statistics (mean, CI, HPDR)
 #'
-#' @param a_chain numeric vector containing the output of the MCMC algorithm for the parameter a
-#' @param level probability corresponding to the level of confidence used for the credible interval and the highest density region
-#' @param title label of the title
-#' @param colors if TRUE  -> use of colors in the graph
+#' @param data dataframe containing the output of the MCMC algorithm
+#' @param position numeric vector containing the position of the column corresponding to the MCMC chains of interest
+#' @param level probability corresponding to the level of confidence
 #' @param GridLength length of the grid used to estimate the density
-#' @param exportFile the name of the file to be saved. If NULL then no graph is saved. 
-#' @param exportFormat the format of the export file : PNG or SVG.
-#' @return a plot with the density of a_chain + +CI + mean + HDR
+#' @param title title of the graph
+#' @param subtitle subtitle of the graph
+#' @param caption caption of the graph
+#' @param labelXaxis label of the x-axis
+#' @param labelYaxis label of the y-axis
+#' @param width width size in units
+#' @param height height size in units
+#' @param units a string recognized by the ggsave function, one of "in", "cm", "mm"
+#' @param x.min minimum x axis value
+#' @param x.max maximum x axis value
+#' @param x.scale one of "calendar" for calendar years, "BP" for years before present, or "elapsed" for time elapsed from a specified origin
+#' @param elapsed.origin.position position of the column to use as the origin for elapsed time calculations
+#' @param y.grid switch for horizontal grid lines
+#' @param file the name of the graph (+ extension) that will be saved if chosen. Null by default.
+#' @param newWindow whether the plot is drawn within a new window or not
+#' @return a plot
 #' @export
-MarginalPlot <- function(a_chain, level=0.95, title="Characteristics of a date", colors=TRUE, exportFile = NULL, exportFormat = "PNG", GridLength=1024){
+#' 
+MarginalPlot <- function(a_chain, level=0.95, title="Characteristics of a date", subtitle = NULL,
+                          GridLength=1024,
+                          caption = "ArchaeoPhases",
+                          x.label = "Calendar year",y.label = NULL,
+                          x.scale = "calendar", elapsed.origin.position = NULL,
+                          x.min = NULL, x.max = NULL, 
+                          y.grid = TRUE,
+                          height = 7, width = 7, units = "in",
+                          file = NULL, newWindow=TRUE){
   
-  #maxValuex <- min(max(density(a_chain, n=GridLength)$x), 2016)
-  maxValuex <- max(density(a_chain, n=GridLength)$x)
-  minValuex <- min(density(a_chain, n=GridLength)$x)
-  
-  # Computing min and max values
-  x = 10^c(0:10)
-  if(minValuex!=0){  
-    c =0
-    for(i in 1:length(x)) { if( abs(minValuex/x[i])>1) {c=c+1}}
-    if(c>3){ minValuex = floor(minValuex/x[c-1])*x[c-1]} else {minValuex = floor(minValuex/x[c])*x[c]}
+  # x.scale can either be "calendar", "BP" or "elapsed" if any other origin that 0 and 1950
+  if (x.scale == "BP") {
+    a_chain <- 1950-a_chain
   }
-  if(maxValuex!=0){  
-    d=0
-    for(i in 1:length(x)) { if( abs(maxValuex/x[i])>1) {d=d+1}}
-    if(d>3){ maxValuex = ceiling(maxValuex/x[d-1])*x[d-1]} else {maxValuex = ceiling(maxValuex/x[d])*x[d]}
+  if (x.scale == "elapsed") {
+    a_chain <- elapsed.origin.position-a_chain
   }
+  # credible interval
+  CR <- CredibleInterval(a_chain, level=level)
+  CR <- c(CR,"y"=0,"yend"=0)
+  # Mean 
+  Mean = mean(a_chain)
+  # new dataframe
+  dataCR = data.frame("Inf" = CR[2], "Sup" = CR[3], "y"=0,"yend"=0, "Mean"=Mean)
   
-  # x-axis
-  middleValuex <- minValuex + ( maxValuex - minValuex ) / 2
-  P1Valuex <- minValuex + ( maxValuex - minValuex ) / 4
-  P3Valuex <- middleValuex + ( maxValuex - minValuex ) / 4
+  h <- ggplot2::ggplot(data = data, ggplot2::aes(x=a_chain))
+  h <- h + ggplot2::geom_density(n = GridLength)
+  h <- h + ggplot2::geom_segment(data = dataCR, ggplot2::aes(x=dataCR[1,1], xend=dataCR[1,2], y = dataCR[1,3], yend = dataCR[1,4], colour="steelblue"), size=3.5, show.legend=F) 
+  h <- h + ggplot2::geom_point(data = dataCR, ggplot2::aes(x=Mean, y = dataCR[1,3]), size = 2)
+  #h <- h + ggplot2::scale_color_manual(values =c('#56B4E9', '#FC4EO7'))
+  h <- h + ggplot2::labs(x = x.label, y = y.label, title = title, subtitle = subtitle, caption = caption)
   
-  # y-axis
-  step <- max(density(a_chain, n=GridLength)$y) /50   # used to draw CI and mean above the curve
-  maxValuey <- max(density(a_chain, n=GridLength)$y)
-  middleValuey <- maxValuey /2
-  
-  if(!is.null(exportFile)) {
-    
-    if(exportFormat == "PNG") {
-      png( filename = paste(exportFile,"png", sep =".") )
-    } 
-    if(exportFormat == "SVG") {
-      svg( filename = paste(exportFile,"svg", sep =".") )
-    }
-    
-  } 
-  if (colors==T){
-    par(mfrow=c(1,1))
-    plot(density(a_chain, n=GridLength), main = title, xlab = "Calendar Year", axes = FALSE, xlim=c(minValuex,maxValuex), ylim=c(0,max(density(a_chain, n=GridLength)$y) + step))
-    # abscissa axis
-    axis(1, at=c(minValuex, P1Valuex, middleValuex, P3Valuex, maxValuex) )
-    # ordinate axis
-    axis(2, at=c(0, middleValuey , maxValuey),labels =c(0, round(middleValuey, 3), round(maxValuey, 3)) )
-    
-    segments(CredibleInterval(a_chain, level)[2], 0, CredibleInterval(a_chain, level)[3], 0, lwd=6, col = 4)
-    points(mean(a_chain), 0 , lwd=6, col = 2)
-    
-    # legend
-    legend(P3Valuex, maxValuey, c("Density", "Credible Interval", "Mean"), lty=c(1, 1, 0), bty="n", pch=c(NA, NA,1), col = c("black","blue","red"), lwd=c(1,6,6), x.intersp=0.5, cex=0.9)
-    
-  } else {
-    par(mfrow=c(1,1))
-    plot(density(a_chain, n=GridLength), main = title, xlab = "Calendar Year", axes = FALSE, xlim=c(minValuex,maxValuex), ylim=c(0,max(density(a_chain, n=GridLength)$y) + step))
-    # abscissa axis
-    axis(1, at=c(minValuex, P1Valuex, middleValuex, P3Valuex, maxValuex) )
-    # ordinate axis
-    axis(2, at=c(0, middleValuey , maxValuey),labels =c(0, round(middleValuey, 3), round(maxValuey, 3)) )
-    
-    segments(CredibleInterval(a_chain, level)[2], 0, CredibleInterval(a_chain, level)[3], 0, lwd=6, lty=1)
-    points(mean(a_chain), 0 , lwd=6, pch=1)
-    
-    # legend
-    legend(P3Valuex, maxValuey, c("Density", "Credible Interval", "Mean"), lty=c(1,1,0), pch=c(NA,NA,1), bty="n", lwd=c(1,6,6), x.intersp=0.5, cex=0.9)
+  if (y.grid==FALSE) {
+    h <- h + ggplot2::theme(panel.grid.major.y=ggplot2::element_blank())
   }
-  if(!is.null(exportFile)) {
-    
-    dev.off()
-  } 
+  # x abscisses
+  if (is.null(x.min) ) {
+    x.min <- min(density(a_chain, n=GridLength)$x)
+  }
+  if (is.null(x.max)) {
+    x.max <- max(density(a_chain, n=GridLength)$x)
+  }
+  h <- h + ggplot2::xlim(x.min, x.max)
+  
+  # export file
+  if (!is.null(file)) {
+    ggplot2::ggsave(filename = file, plot = h, height = height,width = width, units = units)
+  }
+  if(newWindow == TRUE) {
+    dev.new(height = height, width = width)
+  }
+  print(h)
   
 }
