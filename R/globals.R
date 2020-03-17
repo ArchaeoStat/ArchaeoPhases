346fcefe73348dc483b7f05648aeeb8789d7d572
Thomas S. Dye <tsd@tsdye.online>
2020-03-17 12:33:18 -1000

    Add globals.R to silence devtools::check() notes

---
+++ R/globals.R
@@ -0,0 +1,3 @@
+utils::globalVariables(c("x", "..density..", "variable", "value", "xbar",
+                         "c_i_low", "c_i_high", "dens_fill", "dens_xbar",
+                         "dens_low", "dens_high", "year", "y"))
