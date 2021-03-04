636029ebd6b4475c5320126bec5ce25ed40740b6
nfrerebeau <nicolas.frerebeau@u-bordeaux-montaigne.fr>
2021-03-04 16:48:36 +0100

    Reformat DESCRIPTION

---
+++ R/ArchaeoPhases-package.R
@@ -0,0 +1,24 @@
+#' @author
+#' \strong{Full list of authors and contributors} (alphabetic order)
+#'
+#' \tabular{ll}{
+#'  Thomas S. Dye \tab \emph{University of Hawai'i at Manoa, USA} \cr
+#'  Anne Philippe \tab \emph{Université de Nantes, France} \cr
+#'  Marie-Anne Vibet \tab \emph{Université de Nantes, France} \cr
+#' }
+#'
+#' \strong{Package maintainer}
+#'
+#' Anne Philippe\cr
+#' \email{anne.philippe@@univ-nantes.fr}
+#'
+#' Laboratoire de Mathématiques Jean Leray (UMR 6629)\cr
+#' 2, rue de la Houssinière\cr
+#' BP 92208\cr
+#' F-44322 Nantes Cedex 3\cr
+#' France
+#' @name ArchaeoPhases-package
+#' @aliases ArchaeoPhases
+#' @docType package
+#' @keywords internal
+"_PACKAGE"
