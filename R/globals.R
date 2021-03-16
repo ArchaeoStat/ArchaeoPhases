e0f92a13a27d64e23a9efb4dfe76a755fac31521
Thomas S. Dye <tsd@tsdye.online>
2021-03-15 14:48:17 -1000

    Panels for tempo_plot and occurrence_plot
    Added some variables to globals.R to silence check() notes.
    Specified stats::sd in Sensitivity.R, also to silence check() notes.

--- R/globals.R
+++ R/globals.R
@@ -1,3 +1,4 @@
 utils::globalVariables(c("x", "..density..", "variable", "value", "xbar",
                          "c_i_low", "c_i_high", "dens_fill", "dens_xbar",
-                         "dens_low", "dens_high", "year", "y"))
+                         "dens_low", "dens_high", "year", "y", "start",
+                         "end", "y.lab"))
