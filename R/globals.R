f68ae2dba668824c56138eebf193b51083448f7e
Thomas S. Dye <tsd@tsdye.online>
2021-11-18 05:39:35 -1000

    Allen's interval algebra

--- R/globals.R
+++ R/globals.R
@@ -1,4 +1,4 @@
 utils::globalVariables(c("x", "..density..", "variable", "value", "xbar",
                          "c_i_low", "c_i_high", "dens_fill", "dens_xbar",
                          "dens_low", "dens_high", "year", "y", "start",
-                         "end", "y.lab"))
+                         "end", "y.lab", "node", "result"))
