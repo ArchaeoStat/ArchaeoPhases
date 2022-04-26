16083d7187f0bf854946ce4e726a71b7b319be95
nfrerebeau <nicolas.frerebeau@u-bordeaux-montaigne.fr>
2022-04-26 13:06:09 +0200

    Precompile vignettes

---
+++ vignettes/precompile.R
@@ -0,0 +1,3 @@
+# Vignettes that depend on internet access have been precompiled
+knitr::knit("vignettes/chronos.Rmd.orig", output = "vignettes/chronos.Rmd")
+knitr::knit("vignettes/import.Rmd.orig", output = "vignettes/import.Rmd")
