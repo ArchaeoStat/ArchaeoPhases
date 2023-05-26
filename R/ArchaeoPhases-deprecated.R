a5cfbff815f01470891d41dc7869ad66fccd6122
nfrerebeau <nicolas.frerebeau@u-bordeaux-montaigne.fr>
2023-05-26 13:32:14 +0200

    Defunct function from ArchaeoPhases < 2.0

---
+++ R/ArchaeoPhases-deprecated.R
@@ -0,0 +1,6 @@
+#' Deprecated Functions in ArchaeoPhases
+#'
+#' These functions still work but will be removed (defunct) in the next version.
+#' @name ArchaeoPhases-deprecated
+#' @keywords internal
+NULL
