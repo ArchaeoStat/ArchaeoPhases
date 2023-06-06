2d96d924732d50cfef5a2c547fd7b25db6cdd76f
nfrerebeau <nicolas.frerebeau@u-bordeaux-montaigne.fr>
2023-06-06 19:19:27 +0200

    Fix subsetting method for phases

--- R/subset.R
+++ R/subset.R
@@ -41,30 +41,21 @@ setMethod(
   f = "[",
   signature = c(x = "PhasesMCMC"),
   function(x, i, j, k, drop = FALSE) {
-    z <- methods::callNextMethod()
-
-    if (is.null(dim(z)) || isTRUE(drop)) return(z)
-    if (length(dim(z)) == 2) {
-      n <- dim(z)[1L]
-      m <- if (!missing(j)) length(j) else dim(x)[2L]
-      p <- if (!missing(k)) length(k) else dim(x)[3L]
-      z <- array(z, dim = c(n, m, p))
-    }
-
+    z <- x@.Data
     lab <- x@labels
+    itr <- x@iteration
+
+    z <- z[i, j, k, drop = drop]
     if (!missing(i)) {
-      if (is.character(i)) i <- match(i, dimnames(x)[[1L]])
-      if (!is.null(dimnames(x)[[1L]])) dimnames(z)[[1L]] <- dimnames(x)[[1L]][i]
+      if (is.character(i)) i <- match(i, dimnames(x)[1L])
+      # itr <- itr[i]
     }
     if (!missing(j)) {
       if (is.character(j)) j <- match(j, lab)
-      if (!is.null(dimnames(x)[[2L]])) dimnames(z)[[2L]] <- dimnames(x)[[2L]][j]
       lab <- lab[j]
     }
-    if (!missing(k)) {
-      if (is.character(k)) k <- match(k, dimnames(x)[[3L]])
-      if (!is.null(dimnames(x)[[3L]])) dimnames(z)[[3L]] <- dimnames(x)[[3L]][k]
-    }
+
+    if (isTRUE(drop)) return(z)
     methods::initialize(x, z, labels = lab)
   }
 )
