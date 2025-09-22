066dff664ba56b0e172434c86caa5b24f98646d1
nfrerebeau <nicolas.frerebeau@u-bordeaux-montaigne.fr>
2025-09-22 10:12:45 +0200

    Precompile vignettes

--- vignettes/precompile.R
+++ vignettes/precompile.R
@@ -1,4 +1,8 @@
 # Vignettes that depend on internet access have been precompiled
+if (!requireNamespace("ArchaeoData", quietly = TRUE)) {
+  install.packages("ArchaeoData", repos = "https://archaeostat.r-universe.dev")
+}
+
 old_wd <- setwd("./vignettes")
 knitr::knit("ArchaeoPhases.Rmd.orig", output = "ArchaeoPhases.Rmd")
 knitr::knit("import.Rmd.orig", output = "import.Rmd")
